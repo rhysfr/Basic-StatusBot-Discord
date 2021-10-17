199b7b70fc364de819735bed876773e121e26540
rhys <87879237+rhysfr@users.noreply.github.com>
2021-10-17 13:14:18 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import discord # importing discord module
+from discord.ext import commands
+
+client = discord.Client() 
+
+
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='#')) # Change Status Here - Check Github For More Status's
+    
+    print('Connected to bot: {}'.format(client.user.name))  # Console log
+
+client.run('') # put your token here
